5e7501601beda5c30c96faf1e179f7422530f73b
k_senda <k_senda@yumemi.co.jp>
2023-04-18 23:29:28 +0900

    fix: indent

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,9 +2,9 @@ import { FC } from "react";
 import "./Header.css";
 
 const Header: FC = () => {
-    return <div className="header-root">
-        <div className="header-title">ボロノイ MAP</div>
-    </div>
+  return <div className="header-root">
+    <div className="header-title">ボロノイ MAP</div>
+  </div>
 }
 
 export default Header
--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -3,38 +3,53 @@ import { FC, useCallback, useState } from "react";
 import "./Map.css";
 
 const Map: FC = () => {
-    const { isLoaded } = useJsApiLoader({
-        id: "google-map-script",
-        googleMapsApiKey: process.env.REACT_APP_API_KEY,
-    })
+  const { isLoaded, loadError } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.REACT_APP_API_KEY,
+  })
 
-    const [map, setMap] = useState<google.maps.Map | null>(null)
-    const onLoaded = useCallback((map: google.maps.Map) => {
-        setMap(map)
-    }, [])
-    const onUnmounted = useCallback(() => {
-        setMap(null)
-    }, [])
+  const [map, setMap] = useState<google.maps.Map | null>(null)
+  const onLoaded = useCallback((map: google.maps.Map) => {
+    setMap(map)
+  }, [])
+  const onUnmounted = useCallback(() => {
+    setMap(null)
+  }, [])
 
-    return isLoaded ? (
-        <div className="map-container">
-            <GoogleMap
-                center={{
-                    lat: 35.681236,
-                    lng: 139.767125,
-                }}
-                zoom={14}
-                onLoad={onLoaded}
-                onUnmount={onUnmounted}
-                mapContainerStyle={{
-                    width: "100%",
-                    height: "100%",
-                }}
-            >
+  const render = () => {
+    return (
+      <div className="map-container">
+        <GoogleMap
+          center={{
+            lat: 35.681236,
+            lng: 139.767125,
+          }}
+          zoom={14}
+          onLoad={onLoaded}
+          onUnmount={onUnmounted}
+          mapContainerStyle={{
+            width: "100%",
+            height: "100%",
+          }}
+          options={{
+            streetViewControl: false,
+          }}
+        >
 
-            </GoogleMap>
-        </div>
-    ) : <div>map is loading</div>
+        </GoogleMap>
+      </div>
+    )
+  }
+
+  if (loadError) {
+    return <div>Map can't be loaded.</div>
+  }
+
+  if (!isLoaded) {
+    return <div>Map is loading...</div>
+  }
+
+  return render()
 }
 
 export default Map

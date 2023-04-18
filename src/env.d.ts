d6187ecb40794b9dfc9b0ef1280a75412a1b4d6a
k_senda <k_senda@yumemi.co.jp>
2023-04-19 00:15:30 +0900

    add: json data

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       REACT_APP_API_KEY: string
+      REACT_APP_DATA_VERSION: string
       NODE_ENV: 'development' | 'production'
     }
   }

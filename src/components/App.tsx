bfc9f181530789f0b2296e8baceab6442eee4976
k_senda <k_senda@yumemi.co.jp>
2023-04-18 22:33:32 +0900

    init ts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
+import { FC } from 'react';
+import logo from '../img/logo.svg';
 import './App.css';
 
-function App() {
+const APP: FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default APP
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        loader: 'ts-loader',
+        test: /\.tsx?$/,
+        exclude: [
+          '/node_module/'
+        ],
+        options: {
+          configFile: 'tsconfig.json'
+        }
+      }
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".tsx"],
+  },
+  output: {
+    // This is required so workers are known where to be loaded from
+    publicPath: "/voronoi-map/",
+    filename: "bundle.js",
+    path: `${__dirname}/docs`,
+  },
+};
